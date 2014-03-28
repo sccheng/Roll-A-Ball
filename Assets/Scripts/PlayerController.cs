4accaf4378698c8c44df7f746528cc85c8e4a50b
Stephanie <sccheng@cs.utexas.edu>
2014-03-28 01:24:56 -0500

    finished tutorial part 2: player/ball movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+  public float speed;
+  
+  //called before rendering a frame (usually game code goes here)
+  void Update()
+  {
+    
+  }
+  
+  //called before performing any physics calculations
+  void FixedUpdate()
+  {
+    float moveHorizontal = Input.GetAxis("Horizontal");
+    float moveVertical = Input.GetAxis("Vertical");
+    
+    Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+    
+    rigidbody.AddForce(movement * speed * Time.deltaTime);
+  }
+}
