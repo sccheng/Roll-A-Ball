2b15a2373d30978ae2fc5dba93fb4b2ff4155d27
Stephanie <sccheng@cs.utexas.edu>
2014-04-01 17:31:07 -0500

    finished tutorial part 3: camera movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour 
+{
+  public GameObject player;
+  private Vector3 offset;
+
+	// Use this for initialization
+	void Start ()
+  {
+    offset = transform.position;
+	}
+	
+	// lateUpdate is better than update for:
+  //   follow cameras, procedural animation, 
+  //   and gathering last known states
+	void LateUpdate ()
+  {
+    transform.position = player.transform.position + offset;
+	}
+}
